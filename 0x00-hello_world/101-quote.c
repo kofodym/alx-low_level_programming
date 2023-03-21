23a81e2b8c5dd61307eaa1219e9e0e5014120ddf
“Kofoworola <kofodym07@yahoo.com>
2023-03-21 10:22:40 -0700

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - This is the main function
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+	sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"));
+	return (1);
+}
