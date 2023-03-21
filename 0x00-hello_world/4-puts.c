dc02274675944ea9e1498cfe5a307a0226172685
“Kofoworola <kofodym07@yahoo.com>
2023-03-21 06:16:40 -0700

    Displays using  puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
+
 /**
  * This uses puts instead of printf
  * and displays on a new line
- * */
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle.\n");
+	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
 }
