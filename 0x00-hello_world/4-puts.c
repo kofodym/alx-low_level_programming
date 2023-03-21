2bef4466436eedc82cb9fd31cebf8ef739d37209
“Kofoworola <kofodym07@yahoo.com>
2023-03-21 06:12:21 -0700

    Displays using  puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * This uses puts instead of printf
+ * and displays on a new line
+ * */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle.\n");
+	return (0);
+}
