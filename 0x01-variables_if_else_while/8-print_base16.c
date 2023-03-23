aeb7fc1821e19dd4c550d914e85f79ae2edaaced
“Kofoworola <kofodym07@yahoo.com>
2023-03-23 09:10:59 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,5 +8,17 @@
 
 int main(void)
 {
+	int i;
+	char j;
 
+	for (i = 0 ; i < 10 ; i++)
+	{
+		putchar(i +  '0');
+	}
+	for (j = 'a'; j <= 'f' ; j++)
+	{
+		putchar(j);
+	}
+	putchar ('\n');
+	return (0);
 }
