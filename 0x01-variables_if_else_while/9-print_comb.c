2af1536a2babf3b89b8ea26dbc687a7e68b0a665
“Kofoworola <kofodym07@yahoo.com>
2023-03-23 10:30:47 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,18 +10,15 @@ int main(void)
 {
 	int i;
 
-	for (i = 0 ; i < 10 ; i++)
+	for (i = '0' ; i <= '9' ; i++)
 	{
-		if (i == 9)
+		putchar(i);
+		if (i != '9')
 		{
-			putchar(i + '0');
-		}
-		else
-		{
-			putchar(i + '0');
 			putchar(',');
 			putchar(' ');
 		}
 	}
+	putchar('\n');
 	return (0);
 }
