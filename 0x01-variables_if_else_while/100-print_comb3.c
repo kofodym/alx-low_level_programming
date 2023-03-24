29bb02eea9f684f9d1df37bbaddf95ea16853bd9
“Kofoworola <kofodym07@yahoo.com>
2023-03-24 04:04:59 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - This is the main function
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = '0'; i <= '9' ; i++)
+	{
+		for (j = '1' ; j <= '9' ; j++)
+		{
+			if (i < j && i != j)
+			{
+				putchar(i);
+				putchar(j);
+
+				if (i == '8' && j == '9')
+				{
+					putchar('\n');
+				}
+				else
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
