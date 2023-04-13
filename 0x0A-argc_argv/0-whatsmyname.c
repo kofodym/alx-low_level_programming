19539077e61347c1ded1c1db6bdf339699b4c9c2
“Kofoworola <kofodym07@yahoo.com>
2023-04-13 09:21:27 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - The main function
+ * @argc: The number of arguments
+ * @argv: The array
+ *
+ * Return: Always 0 (Success)
+ */
+
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", *argv);
+	return (0);
+}
