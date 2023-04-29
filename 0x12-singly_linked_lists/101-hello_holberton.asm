3fe5934e70145e7c1dab6c526a32dbcb38d5811d
“Kofoworola <kofodym07@yahoo.com>
2023-04-28 22:59:06 -0700

    assembly

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,14 +1,26 @@
-section .data
-	message db 'Hello, Holberton', 0Ah, 0
-	format db '%s', 0Ah, 0
-
-section .text
-	global main
+; File: 101-hello_holberton.asm
+; Auth: Brennan D Baraban
+; Desc: 64-bit assembly program that prints
+;       Hello, Holberton followed by a new line.
 
 extern printf
 
+section .text
+   global main
+
 main:
-	; prepare arguments for printf
-	mov rdi, format
-	mov rsi, message
-	xor rax, rax
+   push rbp
+
+   mov rdi,fmt
+   mov rsi,msg
+   mov rax,0
+   call printf
+
+   pop rbp
+
+   mov rax,0
+   ret
+
+section .data
+   msg: db "Hello, Holberton", 0
+   fmt: db "%s", 10, 0
