26aa57223160e931a6c7cf84760e98eaa4fc4fae
“Kofoworola <kofodym07@yahoo.com>
2023-05-01 09:17:12 -0700

    Free

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,8 +1,8 @@
 #include "lists.h"
 
 /**
- * free_listint2 - frees a linked list
- * @head: pointer to the listint_t list to be freed
+ * free_listint2 - This frees a linked list
+ * @head: It is a pointer
  */
 void free_listint2(listint_t **head)
 {
