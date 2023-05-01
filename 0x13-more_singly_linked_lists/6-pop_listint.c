6139090cd379eb72a254dba95711c8d8ef7517f2
“Kofoworola <kofodym07@yahoo.com>
2023-05-01 09:19:32 -0700

    deletes the head node of a listint_t

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,8 +1,9 @@
 #include "lists.h"
 
 /**
- * pop_listint - deletes the head node of a linked list
- * @head: pointer to the first element in the linked list
+ * pop_listint - a function that deletes the head node of a
+ * listint_t linked list, and returns the head node’s data (n).
+ * @head: It is a pointer
  *
  * Return: the data inside the elements that was deleted,
  * or 0 if the list is empty
