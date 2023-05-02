6bfc8417fc1bad45806c7ebad5e097679e50f22a
“Kofoworola <kofodym07@yahoo.com>
2023-05-02 06:25:55 -0700

    reverse; git push;

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -1,25 +1,28 @@
 #include "lists.h"
 
 /**
- * reverse_listint -  a function that reverses
- * a listint_t linked list
- * @head: The pointer
- * Return: A pointer head
+ * reverse_listint - the function that reverses listint_t
+ * @head: This double pointer
+ *
+ * Return: It is a pointer to the first node of the reversed list
  */
 listint_t *reverse_listint(listint_t **head)
 {
-	listint_t *next = NULL;
-	listint_t *prev = NULL;
+	listint_t *prev, *next;
 
-	while (*head)
+	if (head == NULL || *head == NULL)
+		return (NULL);
+	if ((*head)->next == NULL)
+		return (*head);
+	prev = NULL;
+	while (*head != NULL)
 	{
 		next = (*head)->next;
 		(*head)->next = prev;
 		prev = *head;
 		*head = next;
 	}
-
 	*head = prev;
-
 	return (*head);
 }
+
