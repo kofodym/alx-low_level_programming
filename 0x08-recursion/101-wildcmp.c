3fb4a74705264528a0ea8780fa8f0dcc16964b5b
“Kofoworola <kofodym07@yahoo.com>
2023-05-02 09:14:40 -0700

    101-wildcmp.c update

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,23 +1,31 @@
 #include "main.h"
 
 /**
- * _chk_string – This will check if two strings are identical.
+ * _chk_string - This will check if two strings are identical.
  * @s1: string_1 base address.
  * @s2: string_2 base address.
  * @i: left index.
  * @j: special index. (joker)
  * Return: 1 if s is palindrome, 0 otherwise.
  */
-int _chk_string (char *s1, char *s2, int i, int j)
+int _chk_string(char *s1, char *s2, int i, int j)
 {
 	if (s1[i] == '\0' && s2[j] == '\0')
+	{
 		return (1);
+	}
 	if (s1[i] == s2[j])
-		return (_chk_string (s1, s2, i + 1, j + 1));
+	{
+		return (_chk_string(s1, s2, i + 1, j + 1));
+	}
 	if (s1[i] == '\0' && s2[j] == '*')
-		return (_chk_string (s1, s2, i, j + 1));
+	{
+		return (_chk_string(s1, s2, i, j + 1));
+	}
 	if (s2[j] == '*')
-		return (_chk_string (s1, s2, i + 1, j) || _chk_string (s1, s2, i, j + 1));
+	{
+		return (_chk_string(s1, s2, i + 1, j) || _chk_string(s1, s2, i, j + 1));
+	}
 	return (0);
 }
 /**
@@ -30,5 +38,5 @@ int _chk_string (char *s1, char *s2, int i, int j)
  */
 int wildcmp(char *s1, char *s2)
 {
-	return (_chk_string (s1, s2, 0, 0));
+	return (_chk_string(s1, s2, 0, 0));
 }
